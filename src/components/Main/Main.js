8c433f18271fc332883c2128ef1f8ded53ebca21
Adam Jaks <adamjaks97@gmail.com>
2021-03-27 21:45:19 +0100

    init and first implementations

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.scss';
+import Header from "../Header/Header";
+
+const Main = () => (
+  <div className="Main">
+    <Header/>
+  </div>
+);
+
+Main.propTypes = {};
+
+Main.defaultProps = {};
+
+export default Main;
