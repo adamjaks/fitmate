ce865d70e3dec4544b36fa0984a0ae8734f41005
Adam Jaks <adamjaks97@gmail.com>
2021-06-10 19:42:25 +0200

    basement link

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ if (localStorage.jwtToken) {
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Router>
+            <Router basename={"https://adamjaks.github.io/fitmate"}>
                 <div className="App">
                     <Route exact path="/login" component={LoginPage}/>
                     <Route exact path="/register" component={RegisterPage}/>
